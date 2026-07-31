8adf2fb8781ac58a8bd2d10a079fc94eb8873651
tunglethanh1998 <tung_le_thanh@bita.jp>
2026-07-31 10:21:01 +0700

    feat: max age

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+
+const ASSET_CACHE = `public, max-age=31536000, immutable`;
+const STATIC_ASSET_PATTERNS = ["/images/(.*)"];
+
 const nextConfig: NextConfig = {
   output: "standalone",
+
+    async headers() {
+    return [
+      ...STATIC_ASSET_PATTERNS.map((source) => ({
+        source,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: ASSET_CACHE,
+          },
+        ],
+      })),
+    ];
+  },
 };
 
 export default nextConfig;
